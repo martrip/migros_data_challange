343548130b27b83c0947461ae1a76f74e6eca805
Martina Trippel <martina.trippel@bluewin.ch>
2021-09-26 16:56:25 +0200

    updated needed libraries

--- code/assign_companies.py
+++ code/assign_companies.py
@@ -1,21 +1,3 @@
-# import pandas as pd
-# import geopandas as gpd
-# import numpy as np
-# from scipy.spatial import cKDTree
-# from shapely.geometry import Point
-# from kd_tree import ckdnearest
-
-# #data cleaning
-# df_companies = pd.read_csv('C:/Users/Martina/Documents/Propulsion/migros_data_challange/data/companies_raw_data.csv')
-# df_companies = df_companies.drop(['company_ltd', 'company_lng'], axis=1)
-# df_companies = df_companies.drop_duplicates()
-# df_companies = df_companies.dropna(subset=['company_coordinates']).reset_index()
-# coordinates = [i for i in df_companies['company_coordinates'].str.replace('m', '').str.replace('s', '').str.split(' ')]
-# df_companies['lat_company'] = [float(i[0])+float(i[1])/60+float(i[2])/3600 for i in coordinates]
-# df_companies['lng_company'] = [float(i[4])+float(i[5])/60+float(i[6])/3600 for i in coordinates]
-#
-# df_geofeatures = pd.read_csv('C:/Users/Martina/Documents/Propulsion/migros_data_challange/data/GeoFeatures_Zurich_needed.csv')
-#
 
 def assign_companies(df_features, df_companies):
     from kd_tree import ckdnearest
